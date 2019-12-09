68727ed181e726fad92870c245ef8bd442f4e0f6
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-10 01:56:44 +0200

    drop down menu to avatar in navbar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {
+    visibility: ["responsive", "group-hover"],
+    borderColors: ["responsive", "hover", "focus", "group-hover"],
+  },
+  plugins: []
+}
