cd77733f03198be1b449f21bd5ccaf0fcf384c62
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-10 23:47:27 +0200

    Add routes for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root 'feed#index'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations' },
+                     path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout' }
+
+  authenticated :user do
+    root 'feed#index', as: :authenticated_user
+  end
+
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      inset: {
+        '1unit': '1%',
+        '2unit': '2%',
+        '7unit': '7%',
+        '10unit': '10%',
+        '20unit': '20%',
+        '50unit': '50%',
+      }
+    }
   },
   variants: {
     visibility: ["responsive", "group-hover"],
