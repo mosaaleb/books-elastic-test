81b2aef12d16be41cf7786cc2ce7ffa60ac0e214
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-11 23:24:45 +0200

    Add active storage attached to book

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[username first_name last_name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[username first_name last_name])
   end
 
   # The path used after sign up.
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,16 @@
 class Book < ApplicationRecord
   enum status: { unread: 0, currently_reading: 1, read: 2 }
   # validations
-  validates :title, :description, :isbn, presence: true
+  validates :title, :description, :isbn, :cover, presence: true
+  # regex isbn ^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$
 
   # association
+  has_one_attached :cover, dependent: :destroy
   has_many :book_additions, dependent: :destroy
   has_many :shelves, through: :book_additions
+
+  # instance methods
+  # def sthumbnail
+  #   cover.variant(resize_to_fit: [300, 200])
+  # end
 end
