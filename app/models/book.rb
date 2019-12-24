f823619e9e756d75e9efef14f546535b2c74156c
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-24 23:51:55 +0200

    isbn validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,10 @@
 class Book < ApplicationRecord
   enum status: { unread: 0, currently_reading: 1, read: 2 }
   # validations
-  validates :title, :description, :isbn, :cover, presence: true
-  # regex isbn ^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$
+  validates :title, :description, :cover, presence: true
+  validates :isbn, :isbn13,
+            format: { with: /\A(97(8|9))?\d{9}(\d|X)\Z/ },
+            presence: true
 
   # association
   has_one_attached :cover, dependent: :destroy
@@ -12,7 +14,12 @@ class Book < ApplicationRecord
   has_many :shelves, through: :book_additions
 
   # instance methods
-  # def sthumbnail
+  # def thumbnail
   #   cover.variant(resize_to_fit: [300, 200])
   # end
+
+  def cover_color
+    # source = ImageProcessing::MiniMagick.source(cover).resize('1x1')
+    # image = MiniMagick::Image.new
+  end
 end
