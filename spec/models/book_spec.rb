21e2106f6d946b1470e7b915cfd1580d9a880b1a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-15 23:57:41 +0200

    add book model specs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:author) }
+    it { is_expected.to have_many(:book_additions) }
+    it { is_expected.to have_many(:shelves).through(:book_additions) }
+
+    it 'has one attached file' do
+      expect(described_class.new.cover)
+        .to be_an_instance_of(ActiveStorage::Attached::One)
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:isbn) }
+    it { is_expected.to validate_presence_of(:cover) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:isbn13) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
 end
