382ac802b28ceb94efd1b55058dfbc952cfd6229
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-19 19:11:07 +0200

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+ActiveRecord::Base.connection.reset_pk_sequence!('authors')
+(1..20).each do |i|
+  Author.create!(name: Faker::Book.author)
+end
+p 'authors created'
+
+
+ActiveRecord::Base.connection.reset_pk_sequence!('books')
+Author.all.each do |author|
+  20.times do
+    book = author.books.new(title: Faker::Book.title,
+                            description: Faker::Lorem.paragraph,
+                            edition: Faker::Number.within(range: 1..5),
+                            isbn: Faker::Number.unique.number(digits: 10),
+                            isbn13: "978#{Faker::Number.unique.number(digits: 10)}")
+
+    book.cover.attach(io: File.open('tmp/book-cover.jpg'),
+                      filename: 'bookcover-sample.png',
+                      content_type: 'image/jpg')
+    book.save!
+  end
+end
+p 'books created'
