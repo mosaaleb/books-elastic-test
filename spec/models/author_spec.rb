090d4e53cc3e3dfa58b9c8f20e6111b60a13f7ec
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-24 22:23:17 +0200

    add author model tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  let(:author) { create(:author_with_followers, followers_count: 10) }
+
   describe 'associations' do
     it { is_expected.to have_many(:books) }
+    it { is_expected.to have_many(:followships) }
+    it { is_expected.to have_many(:followers).through(:followships) }
+  end
+
+  describe '#followers' do
+    it 'return correct number of followers' do
+      expect(author.followers.count).to eq 10
+    end
   end
 end
--- test/factories/authors.rb
+++ test/factories/authors.rb
@@ -3,5 +3,15 @@
 FactoryBot.define do
   factory :author do
     name { Faker::Book.author }
+
+    factory :author_with_followers do
+      transient do
+        followers_count { 5 }
+      end
+
+      after :create do |author, evaluator|
+        create_list(:followship, evaluator.followers_count, author: author)
+      end
+    end
   end
 end
