3b74154cb65a692a91580576a1a19ab3d2039582
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-03-04 23:48:18 +0200

    include status from book_addition in shelf books

--- app/models/shelf.rb
+++ app/models/shelf.rb
@@ -5,4 +5,9 @@ class Shelf < ApplicationRecord
   belongs_to :user
   has_many :book_additions, dependent: :destroy
   has_many :books, through: :book_additions
+
+  # instance methods
+  def shelf_books
+    books.select('books.*, book_additions.status')
+  end
 end
