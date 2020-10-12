46602624072344a7a39745d6020dfb8dadc9a55c
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-10-12 22:41:41 +0200

    refactor navbar by creating helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,7 @@
-# TODO: remove gravatar if not needed
 # frozen_string_literal: true
 
 module ApplicationHelper
   def nav_bar
     render 'navbar' if current_user
-  end
-
-  def avatar(user, size: 100)
-    email_digest = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}?&s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: 'avatar-sm')
   end
 end
